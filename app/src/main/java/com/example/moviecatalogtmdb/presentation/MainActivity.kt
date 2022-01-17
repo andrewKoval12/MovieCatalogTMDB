b33a9fc35d5b9ab0a6b004086e54a4eb11019941
andrewKoval12 <andrey.koval1212@gmail.com>
2022-01-17 16:41:37 +0200

    Add domain, data, presentation layers

--- app/src/main/java/com/example/moviecatalogtmdb/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalogtmdb/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moviecatalogtmdb
+package com.example.moviecatalogtmdb.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviecatalogtmdb.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
