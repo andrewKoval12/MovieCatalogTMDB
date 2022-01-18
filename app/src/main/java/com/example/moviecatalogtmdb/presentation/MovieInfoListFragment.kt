b761c60f88735004cc264fe85dfa6fe948bcf539
andrewKoval12 <andrey.koval1212@gmail.com>
2022-01-18 14:44:22 +0200

    Add MovieInfoListFragment

---
+++ app/src/main/java/com/example/moviecatalogtmdb/presentation/MovieInfoListFragment.kt
@@ -0,0 +1,20 @@
+package com.example.moviecatalogtmdb.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MovieInfoListFragment: Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
